c9674b3e5f527d99f88f6af24fbee319d4285c02
S-Anderson2543 <ander4s7@mail.uc.edu>
2025-09-22 13:34:52 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    double summerCost = 31.40;
+    double fallCost = 43.27;
+    double  winterCost = 29.10;
+    double springCost = 55.50;
+    double finalCost = summerCost + fallCost + winterCost + springCost;
+
+    System.out.println("Your summer costs are $" + summerCost);
+    System.out.println("Your fall costs are $" + fallCost);
+    System.out.println("Your winter costs are $" + winterCost);
+    System.out.println("Your spring costs are $" + springCost);
+    System.out.println("In total, your final costs are $" + finalCost);
+}
